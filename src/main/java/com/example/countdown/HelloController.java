4ce4fa734dd7dae69aefa72981b1b29fbbedbde8
Domokos David <domokosdavid4@gmail.com>
2021-12-06 13:22:13 +0100

    period

--- src/main/java/com/example/countdown/HelloController.java
+++ src/main/java/com/example/countdown/HelloController.java
@@ -2,17 +2,25 @@ package com.example.countdown;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+
+import java.time.LocalDate;
+import java.time.Period;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
+    private Button buttonIndit;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Label labelKiir;
+    @FXML
+    private DatePicker datumBevitel;
 
     public void inditas(ActionEvent actionEvent) {
+        LocalDate mai = LocalDate.now();
+        LocalDate megadott = datumBevitel.getValue();
+        Period kulonbseg = mai.until(megadott);
+        labelKiir.setText(kulonbseg.getYears() + " év " + kulonbseg.getMonths() + " hónap " + kulonbseg.getDays() + " nap ");
     }
 }
